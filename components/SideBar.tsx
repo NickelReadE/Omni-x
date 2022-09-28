a878abacf82f541c527254ccc4520e9cae850abe
Top-Kraken <topkyle521@gmail.com>
2022-09-28 21:40:50 +0900

    Update SideBar.tsx

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -887,6 +887,7 @@ const SideBar: React.FC = () => {
                 <div className="flex flex-row w-full space-x-[15px]">
                   <button onClick={() => handleTargetChainChange(5)} className={targetChain === 5 ? 'border border-g-300' : ''}>
                     <img src="/svgs/ethereum.svg" width={24} height={28} />
+                  </button>
                   <button onClick={() => handleTargetChainChange(ChainIds.ETHEREUM)} className={targetChain === ChainIds.ETHEREUM ? 'border border-g-300' : ''}>
                     <img alt={'networkIcon'} src="/svgs/ethereum.svg" width={24} height={28} />
                   </button>
