fe7776f44aec48dc4e2218f5c4959d9c27350887
socr102 <ericdickersonfree127@gmail.com>
2022-09-29 03:30:59 +0900

    updated_collectoin_page

--- types/enum.ts
+++ types/enum.ts
@@ -1,5 +1,12 @@
 export enum SupportedChainId {
     RINKEBY = 10001,
+    ETHEREUM = 5, // 1
+    BINANCE = 97, // 56
+    AVALANCHE = 43113,
+    POLYGON = 80001,
+    ARBITRUM = 421613,
+    OPTIMISM = 420,
+    FANTOM = 4002,
 }
 
 export enum ChainIds {
