c29233e69a15de94b856dc1b776fb84189726a75
JensonCollins <jenson.collins@hotmail.com>
2022-10-12 05:51:48 +0900

    s3 bucket url is changed

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
     API_BASE_URL: process.env.NEXT_DEVELOPMENT ? 'http://localhost:5000/api/v1/' : 'https://api-dev.omni-x.io/api/v1/',
-    API_URL: 'https://omni-x.s3.amazonaws.com/',
+    API_URL: 'https://bucket-26n19n.s3.amazonaws.com/',
     MORALIS_SERVER_URL: process.env.NEXT_MORALIS_SERVER_URL,
     MORALIS_APP_ID: process.env.NEXT_MORALIS_APP_ID,
     MORALIS_SECRET: process.env.NEXT_MORALIS_SECRET
   },
   images: {
-    domains: ['localhost', 'omni-x.s3.amazonaws.com']
+    domains: ['localhost', 'bucket-26n19n.s3.amazonaws.com']
   },
   rewrites: () => {
     return [
