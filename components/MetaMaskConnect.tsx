4f03d3654a7d24f077349811b7498a072bfc68cb
JensonCollins <jenson.collins@hotmail.com>
2022-10-26 04:53:23 +0900

    Switch network button font size

--- components/MetaMaskConnect.tsx
+++ components/MetaMaskConnect.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
-import SwitchButton from './SwitchButton'
+import {useSwitchNetwork} from 'wagmi'
 import { CHAIN_IDS, supportChainIDs } from '../utils/constants'
 import useWallet from '../hooks/useWallet'
 import { CHAIN_TYPE } from '../types/enum'
@@ -9,6 +9,7 @@ const MetaMaskConnect = (): JSX.Element => {
   const { chainId, address, provider } = useWallet()
   const [show, setShow] = useState<boolean>(true)
   const { openConnectModal } = useConnectModal()
+  const {isLoading, pendingChainId, switchNetwork} = useSwitchNetwork()
 
   const isSupportChain = useMemo(() => {
     if (chainId) {
@@ -36,7 +37,17 @@ const MetaMaskConnect = (): JSX.Element => {
       return (
         <div>
           <div>Current network is not supported <br></br> Please switch network into Goerli</div>
-          <div className="flex justify-center mt-12"><SwitchButton chainID={CHAIN_IDS[CHAIN_TYPE.ARBITRUM]} /></div>
+          <div className="flex justify-center mt-12">
+            <button
+              onClick={() => switchNetwork?.(CHAIN_IDS[CHAIN_TYPE.ARBITRUM])}
+              className="rounded-[10px] border border-l-30 bg-[#B444F9] text-white p-2 hover:text-white hover:bg-l-400 hover:border-l-400 hover:fill-white focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-n-100 focus-visible:outline-none active:bg-l-500 active:border-l-500 active:text-l-100 active:ring-0"
+            >
+              <div className="flex items-center justify-center font-semibold px-4 py-1 font-[32px]">
+                {!isLoading && 'Switch network'}
+                {isLoading && pendingChainId === CHAIN_IDS[CHAIN_TYPE.ARBITRUM] && 'Switching...'}
+              </div>
+            </button>
+          </div>
         </div>
       )
     }
