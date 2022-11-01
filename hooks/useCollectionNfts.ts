4f2cdcd377c01f5e3f112d2c8af9d90ea307a6c2
JensonCollins <jenson.collins@hotmail.com>
2022-11-02 06:11:20 +0900

    Infinite scroll

--- hooks/useCollectionNfts.ts
+++ hooks/useCollectionNfts.ts
@@ -11,8 +11,12 @@ export type CollectionTypeFunc = {
 }
 
 const getCollectionNfts = async (col_url: string, page: number, display_per_page: number, sort: string, searchObj: any) => {
-  const {data: nfts} = await collectionsService.getCollectionNFTs(col_url, page, display_per_page, sort, searchObj)
-  return nfts as NFTItem[]
+  const {data: nfts, totalCount, finished} = await collectionsService.getCollectionNFTs(col_url, page, display_per_page, sort, searchObj)
+  return {
+    nfts: nfts as NFTItem[],
+    totalCount,
+    finished
+  }
 }
 
 const useCollectionNfts = (
@@ -23,58 +27,46 @@ const useCollectionNfts = (
   collectionInfo: CollectionType | undefined
 ): CollectionTypeFunc => {
   const [nfts, setNfts] = useState<NFTItem[]>([])
-  const [init, setInit] = useState<boolean>(false)
   const [hasMoreNFTs, setHasMoreNFTs] = useState<boolean>(true)
   const [page, setPage] = useState(0)
+  const [totalNftCount, setTotalNftCount] = useState(0)
 
   useEffect(() => {
     (async () => {
       if (collectionInfo) {
-        const responseNfts = await getCollectionNfts(col_url, page, display_per_page, sort, searchObj)
-        setNfts(responseNfts)
-        // if (page === 0 && hasMoreNFTs === false && init === true) {
-        // } else {
-        //   setNfts(prevNfts => [...prevNfts, ...responseNfts])
-        // }
-      }
-    })()
-  }, [col_url, page, display_per_page, sort, searchObj, collectionInfo])
-
-  useEffect(() => {
-    if ((collectionInfo && nfts.length >= collectionInfo.itemsCnt)/* || finishedGetting */) {
-      setHasMoreNFTs(false)
-    }
-  }, [nfts, collectionInfo])
-
-  useEffect(() => {
-    (async () => {
-      if (collectionInfo) {
-        setInit(true)
-        setHasMoreNFTs(true)
+        const responseNfts = await getCollectionNfts(col_url, 0, display_per_page, sort, searchObj)
+        setNfts(responseNfts.nfts)
         setPage(0)
+        setTotalNftCount(responseNfts.totalCount)
       }
     })()
+  }, [col_url, display_per_page, sort, searchObj, collectionInfo])
+
+  useEffect(() => {
+    if (collectionInfo) {
+      setHasMoreNFTs(true)
+      setPage(0)
+    }
   }, [searchObj, collectionInfo, sort])
 
   const refreshNfts = async () => {
     // const responseNfts = await getCollectionNfts(col_url, page, display_per_page, sort, searchObj)
-    setInit(true)
     setHasMoreNFTs(false)
     setPage(0)
     // setNfts(responseNfts)
   }
 
-  const fetchMoreData = () => {
-    if (!init)
-      return
-    if (collectionInfo && nfts.length >= collectionInfo.itemsCnt) {
-      setInit(true)
+  const fetchMoreData = async () => {
+    if (collectionInfo && nfts.length >= totalNftCount) {
       setHasMoreNFTs(false)
       return
     }
-    setTimeout(() => {
+    setTimeout(async () => {
       if (col_url) {
         setPage(page + 1)
+        const responseNfts = await getCollectionNfts(col_url, (page + 1), display_per_page, sort, searchObj)
+        setNfts(prevNfts => [...prevNfts, ...responseNfts.nfts])
+        setHasMoreNFTs(!responseNfts.finished)
       }
     }, 500)
   }
