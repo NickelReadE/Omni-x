df18511790b34cde66f600f4e9a89953be3e7fd4
JensonCollins <jenson.collins@hotmail.com>
2023-01-16 09:04:52 +0900

    partners image adding

--- components/learn-more/partners.tsx
+++ components/learn-more/partners.tsx
@@ -9,12 +9,15 @@ export default function LearnMorePartners () {
           <div className={'flex justify-around w-full h-[70px]'}>
             <img src={'/images/learn-more/partners/sneaky.png'} alt={'sneaky'} />
             <img src={'/images/learn-more/partners/kanpai_pandas.png'} alt={'kanpai pandas'} />
+            <img src={'/images/learn-more/partners/stargate.png'} width={200} alt={'stargate'} />
           </div>
           <div className={'flex justify-between w-full mt-8 h-[70px]'}>
             <img src={'/images/learn-more/partners/phd.png'} alt={'pontem'} />
             <img src={'/images/learn-more/partners/galxe.png'} alt={'galxe'} />
+            <img src={'/images/learn-more/partners/brex.svg'} alt={'brex'} width={200} height={70} />
           </div>
           <div className={'flex justify-between w-full mt-8 h-[70px]'}>
+            <img src={'/images/learn-more/partners/alchemy.svg'} alt={'alchemy'} width={200} />
             <img src={'/images/learn-more/partners/layerzero.png'} alt={'layerzero'} />
             <img src={'/images/learn-more/partners/polygonstudios.png'} alt={'polygonstudios'} />
           </div>
