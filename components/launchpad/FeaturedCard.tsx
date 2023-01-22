2a204717eaed9d8ce769084978babf37a2a834d4
exakoss <danielkossman@yahoo.com>
2023-01-22 00:28:42 -0500

    fix: styling and picture on launchpad featured card

--- components/launchpad/FeaturedCard.tsx
+++ components/launchpad/FeaturedCard.tsx
@@ -5,21 +5,24 @@ import {LaunchPadType} from '../../types/collections'
 
 export const FeaturedCard = ({collection}: { collection: LaunchPadType }) => {
   return (
-    <div className={'w-[450px] rounded-[12px] bg-[#202020]'}>
+
+    <div className={'w-[450px] rounded-[12px] bg-[#202020] hover:shadow-[0_0_30px_#FFE817] cursor-pointer'}>
       <Link href={`/drops/${collection.col_url}`}>
-        <img
-          className="w-full hover:cursor-pointer rounded-t-[12px]"
-          src={collection.profile_image}
-          alt="NFT"
-        />
-      </Link>
-      <div className="flex flex-col py-4 px-3">
-        <div className={'flex items-center justify-between'}>
-          <TextH3 className={'text-primary-light'}>{collection.name}</TextH3>
-          <TextSH2 className={'text-primary-light'}>1d 8hrs</TextSH2>
+        <div>
+          <img
+            className="w-full rounded-t-[12px]"
+            src={collection.featured_image}
+            alt="NFT drop"
+          />
+          <div className="flex flex-col py-4 px-3">
+            <div className={'flex items-center justify-between'}>
+              <TextH3 className={'text-primary-light'}>{collection.name}</TextH3>
+              <TextSH2 className={'text-primary-light'}>1d 8hrs</TextSH2>
+            </div>
+            <TextBody className={'text-secondary mt-4'}>{collection.description}</TextBody>
+          </div>
         </div>
-        <TextBody className={'text-secondary mt-4'}>{collection.description}</TextBody>
-      </div>
+      </Link>
     </div>
   )
 }
