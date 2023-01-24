0c583166e0857919a7cada59a6ee72b21a47b773
JensonCollins <jenson.collins@hotmail.com>
2023-01-24 21:44:00 +0900

    delta days showing on live drops

--- components/launchpad/FeaturedCard.tsx
+++ components/launchpad/FeaturedCard.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import Link from 'next/link'
 import {TextBody, TextH3, TextSH2} from '../basic'
 import {LaunchPadType} from '../../types/collections'
 
 export const FeaturedCard = ({collection}: { collection: LaunchPadType }) => {
-  return (
+  const timeTillToMintStartTimestamp = useMemo(() => {
+    const deltaTime = (new Date().getTime() / 1000) - collection.mint_start_timestamp
+    const dayInMilliseconds = 60 * 60 * 24
+    return deltaTime / dayInMilliseconds > 1 ? `${Math.floor(deltaTime / dayInMilliseconds)} days` : `${Math.floor(deltaTime / 60 / 60)} hours`
+  }, [collection])
 
+  return (
     <div className={'w-[450px] rounded-[12px] bg-[#202020] hover:shadow-[0_0_30px_#FFE817] cursor-pointer'}>
       <Link href={`/drops/${collection.col_url}`}>
         <div>
@@ -17,7 +22,7 @@ export const FeaturedCard = ({collection}: { collection: LaunchPadType }) => {
           <div className="flex flex-col py-4 px-3">
             <div className={'flex items-center justify-between'}>
               <TextH3 className={'text-primary-light'}>{collection.name}</TextH3>
-              <TextSH2 className={'text-primary-light'}>1d 8hrs</TextSH2>
+              <TextSH2 className={'text-primary-light'}>{timeTillToMintStartTimestamp}</TextSH2>
             </div>
             <TextBody className={'text-secondary mt-4'}>{collection.description}</TextBody>
           </div>
