56a03257444db85f9a124f457008f1d225845c74
JensonCollins <jenson.collins@hotmail.com>
2023-02-14 04:25:45 +0900

    Leaderboard pagination

--- components/common/Pagination.tsx
+++ components/common/Pagination.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import {TextBodyemphasis} from './Basic'
 
-function Pagination({ totalItems, itemsPerPage, currentPage, setCurrentPage }: { totalItems: number, itemsPerPage: number, currentPage: number, setCurrentPage: (page: number) => void }) {
+function Pagination({ totalPage, currentPage, setCurrentPage }: { totalPage: number, currentPage: number, setCurrentPage: (page: number) => void }) {
   const updateCurrentPage = (page: number) => {
     setCurrentPage(page)
   }
 
   const renderPageButtons = () => {
     const pageButtons = []
-    for (let i = 1; i <= totalItems / itemsPerPage; i++) {
+    for (let i = 0; i < totalPage; i++) {
       pageButtons.push(
         <button className={`${currentPage === i ? 'bg-[#303030]' : ''} py-2 px-4 w-[38px] h-[38px]`} key={i} onClick={() => updateCurrentPage(i)}>
           <TextBodyemphasis className={'text-primary-light'}>
-            {i}
+            {i + 1}
           </TextBodyemphasis>
         </button>
       )
