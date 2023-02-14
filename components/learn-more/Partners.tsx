c5028a5f4c8c9a6ae45c935f5c879fd539237f69
exakoss <danielkossman@yahoo.com>
2023-02-14 01:46:48 -0500

    fix: partner images alignment

--- components/learn-more/Partners.tsx
+++ components/learn-more/Partners.tsx
@@ -6,20 +6,19 @@ export default function LearnMorePartners () {
           <span className={'text-xl2 text-primary-light'}>a special thanks to those that got us here</span>
         </div>
         <div className="flex flex-col w-full mt-12 px-10">
-          <div className={'flex justify-around w-full h-[70px]'}>
-            <img src={'/images/learn-more/partners/sneaky.png'} alt={'sneaky'} />
-            <img src={'/images/learn-more/partners/kanpai_pandas.png'} alt={'kanpai pandas'} />
-            <img src={'/images/learn-more/partners/stargate.png'} width={200} alt={'stargate'} />
-          </div>
-          <div className={'flex justify-between w-full mt-8 h-[70px]'}>
-            <img src={'/images/learn-more/partners/phd.png'} alt={'pontem'} />
-            <img src={'/images/learn-more/partners/galxe.png'} alt={'galxe'} />
-            <img src={'/images/learn-more/partners/brex.svg'} alt={'brex'} width={200} height={70} />
-          </div>
-          <div className={'flex justify-between w-full mt-8 h-[70px]'}>
-            <img src={'/images/learn-more/partners/alchemy.svg'} alt={'alchemy'} width={200} />
+          <div className={'flex justify-around w-full h-[70px] space-x-10'}>
             <img src={'/images/learn-more/partners/layerzero.png'} alt={'layerzero'} />
-            <img src={'/images/learn-more/partners/polygonstudios.png'} alt={'polygonstudios'} />
+            <img src={'/images/learn-more/partners/sneaky.png'} alt={'sneaky'} className="m-h" />
+            <img src={'/images/learn-more/partners/phd.png'} alt={'pontem'} />
+          </div>
+          <div className={'flex justify-around w-full mt-8 h-[70px]'}>
+            <img src={'/images/learn-more/partners/galxe-logo-white.svg'} alt={'galxe'} width={300} height={70} />
+            <img src={'/images/learn-more/partners/brex.svg'} alt={'brex'} width={250} height={70} />
+          </div>
+          <div className={'flex justify-around w-full mt-8 h-[70px] space-x-10'}>
+            <img src={'/images/learn-more/partners/polygon-logo-white.svg'} alt={'polygon'} width={300} />
+            <img src={'/images/learn-more/partners/kanpai_pandas.png'} alt={'kanpai pandas'} />
+            <img src={'/images/learn-more/partners/stargate_logo_full.svg'} width={300} alt={'stargate'} />
           </div>
         </div>
       </div>
